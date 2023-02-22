d10fb1369001367798f053482c8ebc3c3ed4b5a0
unknown <gloowerzin@gmail.com>
2023-02-22 14:24:25 -0300

    Revert "Repository"
    This reverts commit 692f5e201b960590ea1c7ec9f992dade9e426bbe.

--- src/main/java/br/com/testando/api/apirest/controller/ProdutosController.java
+++ src/main/java/br/com/testando/api/apirest/controller/ProdutosController.java
@@ -4,15 +4,14 @@ package br.com.testando.api.apirest.controller;
 import br.com.testando.api.apirest.model.Produtos;
 import br.com.testando.api.apirest.repository.CategoriaRepository;
 import br.com.testando.api.apirest.repository.ProdutosRepository;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/Produtos")
@@ -29,37 +28,5 @@ public class ProdutosController {
     @GetMapping
     public ResponseEntity<List<Produtos>> getAll() {
         return ResponseEntity.ok(produtosRepository.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Produtos> getById (@PathVariable Long id) {
-        return produtosRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
-    @GetMapping("/nome/{nome}")
-    public ResponseEntity<List<Produtos>> getByNome(@PathVariable String nome) {
-        return ResponseEntity.ok(produtosRepository.findAllByNomeContainingIgnoreCase(nome));
-    }
-
-    @PostMapping
-    public ResponseEntity<Produtos> put(@Valid @RequestBody Produtos produtos) {
-        if (produtosRepository.existsById(produtos.getId())) {
-            if (categoriaRepository.existsById(produtos.getCategoria().getId()))
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(produtosRepository.save(produtos));
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
-
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-        Optional<Produtos> produtos = produtosRepository.findById(id);
-        if (produtos.isEmpty())
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        produtosRepository.deleteById(id);
     }
 }
